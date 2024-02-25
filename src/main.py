9613fc5ee5445d3f87dcb245a85bf2ba9b439580
joaovictorgarciacoelho@gmail.com <joaovictorgarciacoelho@gmail.com>
2024-02-25 18:13:17 -0300

    Add GetExtract Route BoilerPlate

--- src/main.py
+++ src/main.py
@@ -1,6 +1,12 @@
 from fastapi import FastAPI
+from datetime import datetime
 
-from src.schemas import CreateTransactionPostBody, CreateTransactionResponse
+
+from src.schemas import (
+    CreateTransactionPostBody,
+    CreateTransactionResponse,
+    GetExtractResponse,
+)
 
 app = FastAPI()
 
@@ -12,4 +18,31 @@ def create_transaction(id: int, transaction: CreateTransactionPostBody):
     return {
         "limite": client["limit"],
         "saldo": client["saldo"],
+    }
+
+
+@app.get("/clients/:id/extrato", response_model=GetExtractResponse)
+def get_extract(id: int):
+
+    client = {"limit": 100000, "saldo": -900, "date": datetime.now().isoformat()}
+    return {
+        "saldo": {
+            "total": client["saldo"],
+            "limite": client["limit"],
+            "data_extrato": client["date"],
+        },
+        "ultimas_transacoes": [
+            {
+                "valor": 100,
+                "tipo": "c",
+                "descricao": "Salario",
+                "realizada_em": datetime.now().isoformat(),
+            },
+            {
+                "valor": 100,
+                "tipo": "d",
+                "descricao": "Mercado",
+                "realizada_em": datetime.now().isoformat(),
+            },
+        ],
     }
--- src/schemas.py
+++ src/schemas.py
@@ -16,3 +16,21 @@ class CreateTransactionPostBody(BaseModel):
 class CreateTransactionResponse(BaseModel):
     limite: int
     saldo: int
+
+
+class Balance(BaseModel):
+    total: int
+    data_extrato: str
+    limite: int
+
+
+class Transaction(BaseModel):
+    valor: int
+    tipo: TipoEnum
+    descricao: str
+    realizada_em: str
+
+
+class GetExtractResponse(BaseModel):
+    saldo: Balance
+    ultimas_transacoes: list[Transaction]
